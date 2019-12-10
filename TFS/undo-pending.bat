541e981e399e354ccacac575992196ea6e8d7e7a
Noelcke <noelcket@oregonstate.edu>
2019-12-10 09:01:30 -0800

    added error handling to the undo-pending script

--- TFS/undo-pending.bat
+++ TFS/undo-pending.bat
@@ -1,7 +1,18 @@
+REM Before running this script you must place the location of the tf command in your path variable.
 ECHO OFF
 SET workspace=%1
 SET name=%2
 SET tempfile=.\temp.txt
+
+IF "%name%"=="" (
+    ECHO Useage: undo-pending.bat [workspace] [Username]
+    exit 1
+)
+IF "%workspace%"=="" (
+    ECHO Useage: undo-pending.bat [workspace] [Username]
+    exit 1
+)
+
 
 ECHO "Finding Pending Changes..."
 ECHO tf stat /workspace:%workspace%;%name% 
